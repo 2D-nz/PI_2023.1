e573f8d2273ab7712fa155c7640118f40235f219
lorenamafra <108240782+lorenamafra@users.noreply.github.com>
2023-05-20 11:38:57 -0300

    Add files via upload

--- src/main/java/br/senac/sp/pi/DAO/ProdutosDAO.java
+++ src/main/java/br/senac/sp/pi/DAO/ProdutosDAO.java
@@ -8,7 +8,11 @@ import br.senac.sp.pi.modelProdutos.Produtos;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,14 +33,14 @@ public class ProdutosDAO {
                conexao = DriverManager.getConnection(url, "root", "dede@2102");
                
                PreparedStatement comandoSQL = conexao.prepareStatement("INSERT INTO cadastroproduto" + 
-                       "(peca, cor, tamanho, preco, qtd) VALUES(?,?,?,?,?)");
+                       "(idProduto, peca, cor, tamanho, preco, qtd) VALUES(?,?,?,?,?,?)");
                
-               
-               comandoSQL.setString(1, obj.getPeca());
-               comandoSQL.setString(2, obj.getCor());
-               comandoSQL.setInt(3, obj.getTamanho());
-               comandoSQL.setDouble(4, obj.getPreco());
-               comandoSQL.setInt(5, obj.getQtd());
+               comandoSQL.setInt(1, obj.getIdProduto());
+               comandoSQL.setString(2, obj.getPeca());
+               comandoSQL.setString(3, obj.getCor());
+               comandoSQL.setString(4, obj.getTamanho());
+               comandoSQL.setDouble(5, obj.getPreco());
+               comandoSQL.setInt(6, obj.getQtd());
                
                int linhasAfetadas = comandoSQL.executeUpdate();
                
@@ -52,6 +56,75 @@ public class ProdutosDAO {
         return retorno;
         
         
+    }
+    public static boolean excluir(int IdProduto){
+        
+        boolean retorno = false;
+        Connection conexao = null;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/iclothes";
+               conexao = DriverManager.getConnection(url, "root", "dede@2102");
+               
+               PreparedStatement comandoSQL = 
+                       conexao.prepareStatement("DELETE FROM cadastroproduto WHERE idProduto =?");
+               
+               comandoSQL.setInt(1, IdProduto);
+               
+               int linhasAfetadas = comandoSQL.executeUpdate();
+               
+               if(linhasAfetadas>0){
+                retorno = true;
+            }
+        
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Erro ao carregar o Driver");
+        } catch (SQLException ex) {
+            System.out.println("Erro ao abrir a conexao");
+        }
+        return retorno;
+        
+    } public static ArrayList<Produtos> listar(){
+        ArrayList<Produtos> listaRetorno  = new ArrayList<>();
+        Connection conexao = null;
+        
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            String url = "jdbc:mysql://localhost:3306/iclothes";
+            conexao = DriverManager.getConnection(url, "root", "dede@2102");
+            
+            PreparedStatement comandoSQL = 
+            conexao.prepareStatement("SELECT * FROM cadastroproduto");
+            
+            ResultSet rs = comandoSQL.executeQuery();
+            
+            if(rs!=null){
+                while(rs.next()){
+                    Produtos obj = new Produtos();
+                    obj.setIdProduto(rs.getInt("idProduto"));
+                    obj.setPeca(rs.getString("peca"));
+                    obj.setCor(rs.getString("cor"));
+                    obj.setTamanho(rs.getString("tamanho"));
+                    obj.setPreco(rs.getDouble("preco"));
+                    obj.setQtd(rs.getInt("qtd"));
+                    
+                    listaRetorno.add(obj);
+                    
+                }
+            }
+            
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Erro ao carregar o Driver");
+        } catch (SQLException ex) {
+            System.out.println("Erro ao abrir a conexão");
+        }
+        
+        
+           return listaRetorno;
+       
     }
     
 }
