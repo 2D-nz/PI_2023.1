41a4b56aa1c89bedb6783c7ee795a795e18eb633
lorenamafra <108240782+lorenamafra@users.noreply.github.com>
2023-05-20 22:01:25 -0300

    Add files via upload

--- src/main/java/br/senac/sp/pi/DAO/ProdutosDAO.java
+++ src/main/java/br/senac/sp/pi/DAO/ProdutosDAO.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.senac.sp.pi.daoProdutos;
-
+    
 import br.senac.sp.pi.modelProdutos.Produtos;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,7 +11,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,63 +27,113 @@ public class ProdutosDAO {
         Connection conexao = null;
         
         try {
+            
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/iclothes";
-               conexao = DriverManager.getConnection(url, "root", "dede@2102");
-               
-               PreparedStatement comandoSQL = conexao.prepareStatement("INSERT INTO cadastroproduto" + 
-                       "(idProduto, peca, cor, tamanho, preco, qtd) VALUES(?,?,?,?,?,?)");
-               
-               comandoSQL.setInt(1, obj.getIdProduto());
-               comandoSQL.setString(2, obj.getPeca());
-               comandoSQL.setString(3, obj.getCor());
-               comandoSQL.setString(4, obj.getTamanho());
-               comandoSQL.setDouble(5, obj.getPreco());
-               comandoSQL.setInt(6, obj.getQtd());
-               
-               int linhasAfetadas = comandoSQL.executeUpdate();
-               
-               if(linhasAfetadas>0){
+            
+            
+            conexao = DriverManager.getConnection(url, "root", "dede@2102");
+            
+            PreparedStatement comandoSQL = conexao.prepareStatement("INSERT INTO cadastroproduto " + 
+                    "(idProduto, peca, cor, tamanho, preco, qtd) VALUES(?,?,?,?,?,?)");
+            
+            comandoSQL.setInt(1, obj.getIdProduto());
+            comandoSQL.setString(2, obj.getPeca());
+            comandoSQL.setString(3, obj.getCor());
+            comandoSQL.setString(4, obj.getTamanho());
+            comandoSQL.setDouble(5, obj.getPreco());
+            comandoSQL.setInt(6, obj.getQtd());
+            
+            
+            int linhasAfetadas = comandoSQL.executeUpdate();
+            
+            if(linhasAfetadas>0){
                 retorno = true;
             }
-        
+                    
+            
         } catch (ClassNotFoundException ex) {
             System.out.println("Erro ao carregar o Driver");
         } catch (SQLException ex) {
-            System.out.println("Erro ao abrir a conexao");
+            System.out.println("Erro ao abrir a conexão");
         }
+        
+        
+        
+        
         return retorno;
         
-        
     }
-    public static boolean excluir(int IdProduto){
+    
+    public static ArrayList<Produtos> listar(){
+        ArrayList<Produtos> listaRetorno = new ArrayList<>();
+        Connection conexao = null;
+        
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            String url = "jdbc:mysql://localhost:3306/iclothes";
+            conexao = DriverManager.getConnection(url, "root","dede@2102");
+            
+            PreparedStatement comandoSQL =
+            conexao.prepareStatement("SELECT * FROM cadastroproduto");
+            
+            ResultSet rs = comandoSQL.executeQuery();
+            
+            if(rs!=null){
+                while(rs.next()){
+                    Produtos obj = new Produtos();
+                    obj.setIdProduto(rs.getInt("idProduto"));
+                    obj.setPeca(rs.getString("peca"));
+                    obj.setCor(rs.getString("cor"));
+                    obj.setTamanho(rs.getString("tamanho"));
+                    obj.setPreco(rs.getDouble("preco"));
+                    obj.setQtd(rs.getInt("qtd"));
+                    
+                    listaRetorno.add(obj);
+                }
+            }
+            
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Erro ao carregar o Driver");
+        } catch (SQLException ex) {
+            System.out.println("Erro ao abrir conexão");
+        }
+        
+        return listaRetorno;
+    } public static boolean excluir(int IdProduto){
         
         boolean retorno = false;
         Connection conexao = null;
         
         try {
+            
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/iclothes";
-               conexao = DriverManager.getConnection(url, "root", "dede@2102");
-               
-               PreparedStatement comandoSQL = 
-                       conexao.prepareStatement("DELETE FROM cadastroproduto WHERE idProduto =?");
-               
-               comandoSQL.setInt(1, IdProduto);
-               
-               int linhasAfetadas = comandoSQL.executeUpdate();
-               
-               if(linhasAfetadas>0){
+            
+            
+            conexao = DriverManager.getConnection(url, "root", "dede@2102");
+            
+            PreparedStatement comandoSQL = 
+                    conexao.prepareStatement("DELETE FROM cadastroproduto WHERE idProduto =?");
+            
+            comandoSQL.setInt(1, IdProduto);
+
+            int linhasAfetadas = comandoSQL.executeUpdate();
+            
+            if(linhasAfetadas>0){
                 retorno = true;
             }
-        
+                    
+            
         } catch (ClassNotFoundException ex) {
             System.out.println("Erro ao carregar o Driver");
         } catch (SQLException ex) {
-            System.out.println("Erro ao abrir a conexao");
+            System.out.println("Erro ao abrir a conexão");
         }
-        return retorno;
+ 
         
-    }
-    
+        return retorno;
+}
 }
